5031f24e3bd172f79fd68daf349b8246e8ab40a4
Sonali Priya <sonalipriya@outlook.com>
2017-02-26 12:22:56 -0600

    Foixed the bug

--- app/crack/runGuess.php
+++ app/crack/runGuess.php
@@ -6,6 +6,8 @@ $num  = array_map('intval', str_split($num1));
 $ac=null;
 
 if($num1!=$guessNum) {
+    $perv=0;
+    $i=1;
     $guess = array_map('intval', str_split($guessNum));
     $freqs = array_count_values($num);
     $freqsUser = array_count_values($guess);
@@ -25,9 +27,25 @@ if($num1!=$guessNum) {
                 }
             }else if($freq < $freqUser){
                 if($g==$val){
-                    $ac .= '<span class="glyphicon glyphicon-ok text-success"></span>';
+                    $ac .= '<span class="glyphicon glyphicon-ok text-warning"></span>';
+                    $i++;
                 }else{
-                    $ac .= '<span class="glyphicon glyphicon-transfer text-warning"></span>';
+                    if($freq==1 && $i==1 && $g==$val){
+                        $ac .= '<span class="glyphicon glyphicon-ok text-success"></span>';
+                    }else if($freq==1 && $i==1 && $g!=$val){
+                        $ac .= '<span class="glyphicon glyphicon-transfer text-warning"></span>';
+                    }else if($i<=$freq && $g!=$val) {
+                        $ac .= '<span class="glyphicon glyphicon-remove text-danger"></span>';
+                        $i++;
+                    }else  if($i<=$freq && $g==$val) {
+                          $ac .= '<span class="glyphicon glyphicon-ok text-success"></span>';
+                          $i++;
+                      }
+                    else if($i<=$freqUser && $g!=$val){
+                        $ac .= '<span class="glyphicon glyphicon-remove text-danger"></span>'; //prob
+                    }else if($i<=$freqUser && $g==$val){
+                        $ac .= '<span class="glyphicon glyphicon-transfer text-danger"></span>'; //prob
+                    }
                 }
             }else if($g==$val || ($freq>1 && $g==$val)){
                 $ac .= '<span class="glyphicon glyphicon-ok text-success"></span>';
@@ -36,9 +54,6 @@ if($num1!=$guessNum) {
             }
         }else{
             $ac .= '<span class="glyphicon glyphicon-remove text-danger"></span>';
-        }
-        if($freq == 1 || $freqUser>$freq){
-            $num[$keyAc] = null;
         }
     }
     echo $ac.','.$num1;
